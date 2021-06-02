d89c1dd84e70e7f5b1769ba04b997395ad381dd1
Mahtab Nejad <mahtab@bahmannejad.com>
2021-06-02 23:32:27 +0100

    expressions

--- playground/udemy-courses/Intro-to-Java/HelloWorld/src/Hello.java
+++ playground/udemy-courses/Intro-to-Java/HelloWorld/src/Hello.java
@@ -5,10 +5,21 @@ public class Hello {
 
         //datatype is int. = is an operator and 5 is the value. AKA declaration statement
         int myFirstNumber = (5 + 10) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        //int myLastOne = -883;
+
+        //this is an expression > myFirstNumber + mySecondNumber + myThirdNumber
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
 
         //prints out the value of myFirstNumber variable
         System.out.println("myFirstNumber value is: " + myFirstNumber);
+        //prints out expression
+        System.out.println("myTotal is: " + myTotal);
 
+        // use - operator
+        int myLastOne = 1000 - myTotal;
+        System.out.println("myLastOne is: " + myLastOne);
 
     }
 }
